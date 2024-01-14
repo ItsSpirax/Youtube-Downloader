2ab002dfc8770ddb7a279a04d78095ea5c56d7cb
Adith <60130987+ItsSpirax@users.noreply.github.com>
2024-01-14 22:00:40 +0530

    Write Automatic Subs

--- run.py
+++ run.py
@@ -1,38 +1,39 @@
 import os, subprocess, yt_dlp
 
-url = input("Youtube Video Link: ")
+url = input('Youtube Video Link: ')
 choice = input(
-    "\nDownload Format:\n1 - Audio\n2 - Only Video\n3 - Video with Audio\n\n"
+    '\nDownload Format:\n1 - Audio\n2 - Only Video\n3 - Video with Audio\n\n'
 )
-if choice == "1":
+if choice == '1':
     ydlp_opts = {
-        "outtmpl": "%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp3",
-        "format": "bestaudio[ext=m4a]/bestaudio",
-        "postprocessors": [
+        'outtmpl': '%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp3',
+        'format': 'bestaudio[ext=m4a]/bestaudio',
+        'postprocessors': [
             {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
             }
         ],
     }
-elif choice == "2":
+elif choice == '2':
     ydlp_opts = {
-        "outtmpl": "%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp4",
-        "format": "bestvideo[ext=mp4][height<=1080]/bestvideo",
-        "writethumbnail": True,
-        "postprocessors": [{"key": "FFmpegMetadata"}, {"key": "EmbedThumbnail"}],
+        'outtmpl': '%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp4',
+        'format': 'bestvideo[ext=mp4][height<=1080]/bestvideo',
+        'writethumbnail': True,
+        'postprocessors': [{'key': 'FFmpegMetadata'}, {'key': 'EmbedThumbnail'}],
     }
 else:
     ydlp_opts = {
-        "outtmpl": "%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp4",
-        "format": "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/bestvideo+bestaudio",
-        "writesubtitles": True,
-        "subtitle": "--write-auto-sub --sub-lang en",
-        "writethumbnail": True,
-        "postprocessors": [
-            {"key": "FFmpegMetadata"},
-            {"key": "FFmpegEmbedSubtitle"},
-            {"key": "EmbedThumbnail"},
+        'outtmpl': '%USERPROFILE%/Downloads/YouTube Downloads/%(title)s.mp4',
+        'format': 'bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/bestvideo+bestaudio',
+        'writesubtitles': True,
+        'writeautomaticsub': True,
+        'subtitleslangs': ['en', 'auto'],
+        'writethumbnail': True,
+        'postprocessors': [
+            {'key': 'FFmpegMetadata'},
+            {'key': 'FFmpegEmbedSubtitle'},
+            {'key': 'EmbedThumbnail'},
         ],
     }
 
